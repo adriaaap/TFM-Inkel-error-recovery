ab26c8bd6f10f4d1b40ddf5b0fae38e0a323d70c
adriaaap <adria_aap@hotmail.com>
2020-08-23 19:55:37 +0200

    Error generator now sends errors at random intervals to one of the available signals chosen at random

--- error_generator.vhd
+++ error_generator.vhd
@@ -1,5 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE ieee.math_real.all;
 
 ENTITY error_generator IS 
 	PORT(clk : IN STD_LOGIC;
@@ -30,41 +32,77 @@ ENTITY error_generator IS
 END error_generator;
 
 ARCHITECTURE structure OF error_generator IS
-	SIGNAL error_clock : integer range 0 to 10;
+
+	CONSTANT min_error_cycles : integer := 3;
+	CONSTANT max_error_cycles : integer := 6;
+	CONSTANT number_of_signals : integer := 19;
+
+	SIGNAL error_clock : integer;
+	SIGNAL tick_number : integer;
+	SIGNAL signal_number : integer;
+	SIGNAL inject_error : STD_LOGIC;
+
 
 BEGIN
-   	p : PROCESS(clk)
-    	BEGIN
+
+	genRandInt : PROCESS(clk)
+	    VARIABLE seed1, seed2 : integer := 42;
+	    IMPURE FUNCTION rand_int(min_val, max_val : integer) return integer is
+		variable r : real;
+	    BEGIN
+		uniform(seed1, seed2, r);
+		return integer(round(r * real(max_val - min_val + 1) + real(min_val) - 0.5));
+	    END FUNCTION;
+	BEGIN
+
+	-- We need to do something here with the random numbers--
 	    IF rising_edge(clk) THEN
+		-- If RESET, generate a new countdown value and decide which signal will get the error
 		IF reset = '1' THEN
 		    error_clock <= 0;
-		ELSIF error_clock = 10 THEN
+		    tick_number <= rand_int(min_error_cycles, max_error_cycles);
+		    signal_number <= rand_int(0, number_of_signals-1);
+		    inject_error <= '0';
+		-- Before reaching the final countdown value, inject an error in the chosen signal
+		ELSIF error_clock = tick_number - 1 THEN
+		    inject_error <= '1';
+		    error_clock <= error_clock + 1;
+		-- After an error is generated, generate a new countdown value and decide which signal will get the next error
+		ELSIF error_clock = tick_number THEN
 		    error_clock <= 0;
+		    tick_number <= rand_int(min_error_cycles, max_error_cycles);
+		    signal_number <= rand_int(0, number_of_signals-1);
+		    inject_error <= '0';
+		-- Increase the counter this cycle, nothing else happens
 		ELSE
 		    error_clock <= error_clock + 1;
+		    inject_error <= '0';
 		END IF;
 	    END IF;
-	END PROCESS p;
 
-	D_inst_type_err <= '1' WHEN error_clock = 5 else '0';
-	D_op_code_err <= '0';
-	D_reg_src1_err <= '0';
-	D_reg_src2_err <= '0';
-	D_reg_dest_err <= '0';
-	D_inm_ext_err <= '0';
-	D_ALU_ctrl_err <= '0';
-	D_branch_err <= '0';
-	D_branch_if_eq_err <= '0';
-	D_jump_err <= '0';
-	D_reg_src1_v_err <= '0';
-	D_reg_src2_v_err <= '0';
-	D_inm_src2_v_err <= '0';
-	D_mem_write_err <= '0';
-	D_byte_err <= '0';
-	D_mem_read_err <= '0';
-	D_reg_we_err <= '0';
-	D_iret_err <= '0';
-	D_invalid_inst_err <= '0';
+
+	END PROCESS genRandInt;
+
+
+	D_inst_type_err <= '1' WHEN inject_error = '1' AND signal_number = 0 else '0';
+	D_op_code_err <= '1' WHEN inject_error = '1' AND signal_number = 1 else '0';
+	D_reg_src1_err <= '1' WHEN inject_error = '1' AND signal_number = 2 else '0';
+	D_reg_src2_err <= '1' WHEN inject_error = '1' AND signal_number = 3 else '0';
+	D_reg_dest_err <= '1' WHEN inject_error = '1' AND signal_number = 4 else '0';
+	D_inm_ext_err <= '1' WHEN inject_error = '1' AND signal_number = 5 else '0';
+	D_ALU_ctrl_err <= '1' WHEN inject_error = '1' AND signal_number = 6 else '0';
+	D_branch_err <= '1' WHEN inject_error = '1' AND signal_number = 7 else '0';
+	D_branch_if_eq_err <= '1' WHEN inject_error = '1' AND signal_number = 8 else '0';
+	D_jump_err <= '1' WHEN inject_error = '1' AND signal_number = 9 else '0';
+	D_reg_src1_v_err <= '1' WHEN inject_error = '1' AND signal_number = 10 else '0';
+	D_reg_src2_v_err <= '1' WHEN inject_error = '1' AND signal_number = 11 else '0';
+	D_inm_src2_v_err <= '1' WHEN inject_error = '1' AND signal_number = 12 else '0';
+	D_mem_write_err <= '1' WHEN inject_error = '1' AND signal_number = 13 else '0';
+	D_byte_err <= '1' WHEN inject_error = '1' AND signal_number = 14 else '0';
+	D_mem_read_err <= '1' WHEN inject_error = '1' AND signal_number = 15 else '0';
+	D_reg_we_err <= '1' WHEN inject_error = '1' AND signal_number = 16 else '0';
+	D_iret_err <= '1' WHEN inject_error = '1' AND signal_number = 17 else '0';
+	D_invalid_inst_err <= '1' WHEN inject_error = '1' AND signal_number = 18 else '0';
 
 
 END structure;
